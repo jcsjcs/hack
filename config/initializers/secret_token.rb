79b600aa60d36ad354787b32e4e8c8fdf0d099db
James Silberbauer <jamessil@telkomsa.net>
2013-09-13 20:32:17 +0200

    Initial checkin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hack::Application.config.secret_key_base = '5749d3bbe88ca3769b89836a63c6c97527a7c75259d2518dedb16b589273ebd8ad09d6f14799a2478f1486d01cdf7c8fbe4a6bfd904d3deed73d1ceb949160c8'
